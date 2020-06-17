10ac49c8cd632b26706a4bbc326a2b7fd6291ffd
alvalle09 <phredvalle@gmail.com>
2020-06-17 00:19:21 -0500

    simulate error for debugging practice

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   console.log("App executed.");
+  let x;
+  x.toString();
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+      <Text>Hello React Native!!!!</Text>
     </View>
   );
 }
